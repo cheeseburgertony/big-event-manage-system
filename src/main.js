b1a016514c26065431c0162e88b737c494b9f30a
cheeseburgertony <1821955020@qq.com>
2024-06-11 17:51:11 +0800

    调整项目目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import '@/assets/main.scss'
 
 const app = createApp(App)
 
