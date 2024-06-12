8529fadb57874721b1caebbcd0adaf3f040b808b
cheeseburgertony <1821955020@qq.com>
2024-06-12 09:58:24 +0800

    完成登录注册页面的静态结构

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,11 @@
 // const router = useRouter()
 // 2.获取路由参数 route 要使用 useRoute 方法 这个方法从vue-router导入
 // const route = useRoute()
-import { useRouter, useRoute } from 'vue-router'
-import { useUserStore, useCounterStore } from '@/stores'
-const router = useRouter()
-const route = useRoute()
-const goList = () => {
-  console.log(router, route)
-}
-
-const userStore = useUserStore()
-const counterStore = useCounterStore()
 </script>
 
 <template>
   <div>
-    <hr />
-    <hr />
     <router-view></router-view>
-    <hr />
-    <hr />
-    App
-    <button @click="$router.push('/home')">跳首页</button>
-    <button @click="goList">跳列表页</button>
-    <TestCom> </TestCom>
-    <el-button type="success" plain>Success</el-button>
-    <el-button type="primary" plain>Primary</el-button>
-
-    <div>
-      <p>{{ userStore.token }}</p>
-      <el-button @click="userStore.setToken('asasacacascsaa')">
-        登录
-      </el-button>
-      <el-button @click="userStore.removeToken"> 退出 </el-button>
-      <p>{{ counterStore.count }}</p>
-      <button @click="counterStore.add(2)">点击+2</button>
-    </div>
   </div>
 </template>
 
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,1 +1,128 @@
-<template>登录页面</template>
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const isRegister = ref(true)
+</script>
+
+<template>
+  <!-- 
+    el-row表示一行 一行分为24份
+    el-col表示列
+    (1) :span="12" 表示这一列占这一行的12份  (50%)
+    (2) :span="6" 表示这一列占这一行的6份  (25%)
+    (3) :offset="3" 表示这一列在这一行中左侧的margin份数
+
+    el-form 表示整个表单组件
+    el-form-item 表单的一行 (一个表单域)
+    el-input 表单元素 (输入框)
+  -->
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <!-- 注册表单 -->
+      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入再次密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+
+      <!-- 登录表单 -->
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            登录
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+
+  .bg {
+    background:
+      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+
+    .title {
+      margin: 0 auto;
+    }
+
+    .button {
+      width: 100%;
+    }
+
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
