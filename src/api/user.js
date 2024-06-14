00af2aa0c1390a5a2a20b78763dc616e5d5c34e2
cheeseburgertony <1821955020@qq.com>
2024-06-14 13:53:47 +0800

    完成更新头像功能

--- src/api/user.js
+++ src/api/user.js
@@ -17,3 +17,7 @@ export const userGetInfoService = () => request.get('/my/userinfo')
 
 // 更新用户信息
 export const userUpdateInfoService = (data) => request.put('/my/userinfo', data)
+
+// 更新用户头像
+export const userUpdateAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
--- src/views/user/UserAvater.vue
+++ src/views/user/UserAvater.vue
@@ -1,1 +1,92 @@
-<template>更换头像</template>
+<script setup>
+import { ref } from 'vue'
+import { Plus, Upload } from '@element-plus/icons-vue'
+import { useUserStore } from '@/stores'
+import { userUpdateAvatarService } from '@/api/user'
+// 用于点击按钮触发选择图片
+const uploadRef = ref()
+const userStore = useUserStore()
+// 基于userStore的值进行渲染头像
+const imgUrl = ref(userStore.user.user_pic)
+
+// 选择图片后进行回显
+const onSelectFile = (upLoadFile) => {
+  // 基于FileReader做图片预览
+  const reader = new FileReader()
+  reader.readAsDataURL(upLoadFile.raw)
+  reader.onload = () => {
+    imgUrl.value = reader.result
+  }
+}
+
+// 提交到服务器更新用户头像
+const onUpdateAvatar = async () => {
+  await userUpdateAvatarService(imgUrl.value)
+  // 更新用户信息
+  userStore.getUser()
+  // 给用户提示
+  ElMessage.success('头像更新成功')
+}
+</script>
+
+<template>
+  <page-container title="更换头像">
+    <el-upload
+      ref="uploadRef"
+      class="avatar-uploader"
+      :auto-upload="false"
+      :show-file-list="false"
+      :on-change="onSelectFile"
+    >
+      <img v-if="imgUrl" :src="imgUrl" class="avatar" />
+      <el-icon v-else class="avatar-uploader-icon">
+        <Plus />
+      </el-icon>
+    </el-upload>
+    <br />
+    <el-button
+      @click="uploadRef.$el.querySelector('input').click()"
+      type="primary"
+      :icon="Plus"
+      size="large"
+      >选择图片</el-button
+    >
+    <el-button
+      @click="onUpdateAvatar"
+      type="success"
+      :icon="Upload"
+      size="large"
+      >上传头像</el-button
+    >
+  </page-container>
+</template>
+
+<style lang="scss" scoped>
+.avatar-uploader {
+  :deep() {
+    .avatar {
+      width: 278px;
+      height: 278px;
+      display: block;
+    }
+    .el-upload {
+      border: 1px dashed var(--el-border-color);
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+      transition: var(--el-transition-duration-fast);
+    }
+    .el-upload:hover {
+      border-color: var(--el-color-primary);
+    }
+    .el-icon.avatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 278px;
+      height: 278px;
+      text-align: center;
+    }
+  }
+}
+</style>
